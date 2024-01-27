2a78add3728fdb4ed6c0c6bd5dc9a1e1d26f147e
Angel Lizardo <aahlizardo.britanico@gmail.com>
2024-01-26 23:13:59 -0600

    First Objective of the challenge created, further testing needed.

--- Retos/RetoDos.java
+++ Retos/RetoDos.java
@@ -1,7 +1,42 @@
 package Retos;
 
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class RetoDos {
     public static void exec() {
-        System.out.println("HELLO WORLD");
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Elija una de las siguientes opciones:\n1. Calculadora de promedio final\n2. Programa de venta de cajas");
+        switch(entrada.nextInt()){
+            case 1->{
+                int nCal;
+                float promedio = 0;
+                Formatter promFinal = new Formatter();
+                // entrada de datos
+                System.out.println("¿Cuantas calificaciones desea promediar?: ");
+                nCal = entrada.nextInt();
+                // Ingreso de calificaciones
+                for(int i = 1; i <= nCal; i++){
+                    System.out.println("Ingrese la calificacion #" + i + ": ");
+                    promedio += entrada.nextFloat();
+                }
+                // Calculo de promedio y formato del resultado
+                promedio /= nCal;
+                promFinal.format("%.1f", promedio);
+                // Salida de datos
+                System.out.println("El promedio final del alumno es de: " + promFinal.toString());
+                promFinal.close();
+            }
+            case 2->{
+
+            }
+        }
+        
+        entrada.close();
     }
+
+    //private class puntoDeVentaCajas{
+        
+    //}
 }
