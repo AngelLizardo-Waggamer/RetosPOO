5dd699925e16650cdaacd60a5754e02e13569f66
Angel Lizardo <aahlizardo.britanico@gmail.com>
2024-01-28 20:57:40 -0600

    Make the classes and initialize Scanner

--- Retos/RetoTres.java
+++ Retos/RetoTres.java
@@ -1,7 +1,17 @@
 package Retos;
 
+import java.util.Scanner;
+
 public class RetoTres {
+    private static Scanner entrada;
     public static void exec(){
-        
+        entrada = new Scanner(System.in);
+    }
+
+    private class Diccionario{
+
+    }
+    private class juegoNumeros{
+
     }
 }
