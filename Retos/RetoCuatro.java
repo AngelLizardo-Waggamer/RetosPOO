b874b7b1d1f82fb99d0cc7cb6b80ad2ca3d4382d
Angel Lizardo <aahlizardo.britanico@gmail.com>
2024-02-03 22:29:58 -0600

    work in RetoCuatro.java

--- Retos/RetoCuatro.java
+++ Retos/RetoCuatro.java
@@ -75,7 +75,15 @@ public class RetoCuatro {
             System.out.println("Numero de pasajeros adicionales: ");
             pasajerosAdicionales = entrada.nextInt();
             if(pasajerosAdicionales != 0){
-                
+                String[] datosVenta = new String[pasajerosAdicionales + 2];
+                datosVenta[0] = Float.toString(1250.5f * (pasajerosAdicionales + 1));
+                datosVenta[1] = comprador;
+                for(int p = 2; p <= (pasajerosAdicionales + 2); p++){
+                    System.out.println("Nombre del pasajero adicional #" + p + ": ");
+                    datosVenta[p] = entrada.next();
+                }
+                ventas.add(datosVenta);
+                System.out.println("Venta numero " + ventas.size() + " registrada exitosamente.");
             }else{
                 String[] datosVenta = new String[2];
                 datosVenta[0] = Float.toString(1250.5f);
@@ -89,7 +97,11 @@ public class RetoCuatro {
          */
         private void imprimirReporte(){
             for(int i = 0; i<ventas.size(); i++){
-                System.out.println("Venta numero #" + i);
+                String[] temp = ventas.get(i);
+                System.out.println("* Venta numero #" + i + ": ");
+                System.out.println("    Pasajero: " + temp[1]);
+                System.out.println("    Importe: " + "$" + temp[0]);
+                System.out.println("    Pasajeros adicionales: ");
             }
         }
     }
