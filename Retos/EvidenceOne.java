bfcfb6cf2cc8ca6e59233eaf32fa8d38e4f5e63d
unknown <Angel Lizardo>
2024-02-12 08:13:31 -0600

    Add code for EvidenceOne

--- Retos/EvidenceOne.java
+++ Retos/EvidenceOne.java
@@ -3,9 +3,10 @@ package Retos;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
- * EvidenceOne
+ * EvidenceOne.
  */
 public class EvidenceOne {
     private static EvidenceOne obC = new EvidenceOne();
@@ -17,14 +18,22 @@ public class EvidenceOne {
         entrada.close();
     }
     /**
-     * Class to execute the program
+     * Class to execute the program.
      */
     private class programa{
+        private ArrayList<Medicamento> listaMedicamentos = new ArrayList<Medicamento>();
         /**
          * Main method to start the program.
          */
         public void start() throws IOException{
             iniciarSesion("admin", "admin");
+            do{
+                nuevoRegistro();
+                if(!menu()){
+                    break;
+                }
+            }while(menu());
+            mostrarLista();
         }
         /**
          * Inputs the user and password from the user, if they are correct the program continues. If not, the program will ask again until the correct credentials are proportioned.
@@ -41,11 +50,55 @@ public class EvidenceOne {
                 tempPs = entrada.readLine();
                 if(tempUs.equals(user) && tempPs.equals(pass)){
                     System.err.println("Ha iniciado sesión correctamente.");
+                    System.out.println("--------- REGISTRO DE MEDICAMENTOS ---------");
                 }else{
                     System.err.println("Intente de nuevo con las credenciales correctas.");
                 }
             }
-            
+        }
+        /**
+         * Prompts the user if they want to repeat the process of generating a new registry.
+         * @return True if the user inputs {@code S}, False if the user inputs {@code N}
+         * @throws IOException Because of the use of BufferedReader
+         */
+        private boolean menu() throws IOException{
+            System.out.println("¿Desea agregar un nuevo medicamento? (S/N)");
+            if(entrada.readLine().charAt(0) == 'S'){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        /**
+         * Generates a new register.
+         * @throws IOException Because of the use of BufferedReader
+         */
+        private void nuevoRegistro() throws IOException{
+            String nQ, nG, nR;
+            float pP;
+            char tP;
+            // Start the prompt
+            System.out.println("Ingrese el nombre químico del medicamento: ");
+            nQ = entrada.readLine();
+            System.out.println("Ingrese el nombre genérico del medicamento: ");
+            nG = entrada.readLine();
+            System.out.println("Ingrese el nombre registrado del medicamento: ");
+            nR = entrada.readLine();
+            System.out.println("Ingrese el precio al publico del medicamento: ");
+            pP = Float.parseFloat(entrada.readLine());
+            System.out.println("Ingrese el tipo de presentacion.\nA. Sólido\nB.Semisólido\nC.Líquido\nNota: De no darse un valor válido se tomará como No definido");
+            tP = entrada.readLine().charAt(0);
+            Medicamento nMEd = new Medicamento(nQ, nG, nR, pP, tP); // Create medicament object
+            listaMedicamentos.add(nMEd); // Add the object to the list
+        }
+        /**
+         * Shows the medicament list with the details of each one.
+         */
+        private void mostrarLista(){
+            for(int i = 0; i < listaMedicamentos.size(); i++){
+                System.out.println("--------- MEDICAMENTO " + (i + 1) + " ---------");
+                listaMedicamentos.get(i).datos();
+            }
         }
     }
     /**
@@ -63,25 +116,53 @@ public class EvidenceOne {
         }
         /**
          * Constructor for the class Medicamento.
-         * @param nQ
-         * @param nG
-         * @param nR
-         * @param pP
-         * @param pV
-         * @param t_presentacion
+         * @param nQ {@code String} Nombre Químico
+         * @param nG {@code String} Nombre Genérico
+         * @param nR {@code String} Nombre Registrado
+         * @param pP {@code Float} Precio al público
+         * @param t_presentacion {@code Char} tipo de presentación
          */
-        public Medicamento(String nQ, String nG, String nR, float pP, float pV, char t_presentacion){
+        public Medicamento(String nQ, String nG, String nR, float pP, char t_presentacion){
             this.nombreQuimico = nQ;
             this.nombreGenerico = nG;
             this.nombreRegistrado = nR;
             this.precioPublico = pP;
-            this.precioVenta = pV;
-            this.presentacion = switch(t_presentacion){
-                case 'A'->Presentaciones.SOLIDO;
-                case 'B'->Presentaciones.SEMISOLIDO;
-                case 'C'->Presentaciones.LIQUIDO;
-                default->Presentaciones.NODEF;
+            switch(t_presentacion){
+                case 'A'->{
+                    this.presentacion = Presentaciones.SOLIDO;
+                    this.precioVenta = precioPublico + (precioPublico * 0.09f);
+                }
+                case 'B'->{
+                    this.presentacion = Presentaciones.SEMISOLIDO;
+                    this.precioVenta = precioPublico + (precioPublico * 0.12f);
+                }
+                case 'C'->{
+                    this.presentacion = Presentaciones.LIQUIDO;
+                    this.precioVenta = precioPublico + (precioPublico * 0.13f);
+                }
+                default->{
+                    this.presentacion = Presentaciones.NODEF;
+                    this.precioVenta = precioPublico + (precioPublico * 0.1f);
+                }
+            }
+        }
+        /**
+         * Prints all the data from the object.
+         */
+        public void datos(){
+            String pres;
+            System.out.println("Nombre Químico: " + nombreQuimico);
+            System.out.println("Nombre Genérico: " + nombreGenerico);
+            System.out.println("Nombre Registrado: " + nombreRegistrado);
+            System.out.println("Precio al público: " + precioPublico);
+            System.out.println("Precio de venta: " + precioVenta);
+            pres = switch(presentacion){
+                case Presentaciones.SOLIDO->"Sólido";
+                case Presentaciones.LIQUIDO->"Líquido";
+                case Presentaciones.SEMISOLIDO->"Semi Sólido";
+                default->"No definido";
             };
+            System.out.println("Presentación: " + pres);
         }
     }
 }
